08cad29a107f0be755bf1cca28f332fe4d2c2a06
Matthew Behrens & Derick Manliclic <pair+MatthewBehrens.dmanliclic715@devbootcamp.com>
2016-09-14 16:49:27 -0500

    User registration and validation

---
+++ shattlebip/spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "User" do
+  context 'On valid user creation' do
+    let(:user) {User.new(username: "Test Person", email: "Test@Test.com", password_digest: "password1234")}
+
+    it "is a user object" do
+      expect(user.class).to eq User
+    end
+
+    it "has a username" do
+      expect(user.username).to eq "Test Person"
+    end
+
+    it "has a email" do
+      expect(user.email).to eq "Test@Test.com"
+    end
+
+    it "has a password_digest" do
+      expect(user.password_digest).to eq "password1234"
+    end
+  end
+
+  context 'On invalid user creation' do
+
+    it "doesnt create a user" do
+      User.create
+      expect(User.count).to eq 0
+    end
+  end
+end
+
