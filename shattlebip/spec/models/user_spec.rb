22e72d7dc2d08e2775f8b4a67650b706e912e8ce
Matthew Behrens & Derick Manliclic <pair+MatthewBehrens.dmanliclic715@devbootcamp.com>
2016-09-14 17:09:54 -0500

    Users feature done

--- shattlebip/spec/models/user_spec.rb
+++ shattlebip/spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User" do
   context 'On valid user creation' do
-    let(:user) {User.new(username: "Test Person", email: "Test@Test.com", password_digest: "password1234")}
+    let(:user) {User.new(username: "Test Person", email: "Test@Test.com", password: "password1234")}
 
     it "is a user object" do
       expect(user.class).to eq User
@@ -17,7 +17,7 @@ describe "User" do
     end
 
     it "has a password_digest" do
-      expect(user.password_digest).to eq "password1234"
+      expect(user.password_digest.class).to eq String
     end
   end
 
@@ -25,6 +25,11 @@ describe "User" do
 
     it "doesnt create a user" do
       User.create
+      expect(User.count).to eq 0
+    end
+
+    it "doesnt create user if password is less than 6 characters" do
+      User.create(username: "Test Person", email: "Test@Test.com", password: "pass")
       expect(User.count).to eq 0
     end
   end
