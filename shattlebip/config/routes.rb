80825e738b999d73ad4b6aeeb46892f1df9490f7
Laurie Skelly <laurieskelly@users.noreply.github.com>
2016-09-15 14:17:49 -0500

    what i have so far

--- shattlebip/config/routes.rb
+++ shattlebip/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :boards, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
