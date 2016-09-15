f4ee9ed40f4473db8ffd9dc2f79708763f796226
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-15 16:14:04 -0500

    Add board creation on game creation, Add error throw / validations on invalid game creation

--- shattlebip/app/controllers/games_controller.rb
+++ shattlebip/app/controllers/games_controller.rb
@@ -7,6 +7,7 @@ class GamesController < ApplicationController
     @game = Game.new(game_params)
 
     if @game.save
+      @board = Board.create!(user_id: session[:user_id], game_id: @game.id)
       redirect_to games_path, notice: 'Game was successfully created.'
     else
       render :new
--- shattlebip/app/models/game.rb
+++ shattlebip/app/models/game.rb
@@ -1,7 +1,9 @@
 class Game < ActiveRecord::Base
   has_many :boards
   has_many :users, through: :boards
+  
 
+  validates :name, presence: true
   #VALIDATIONS FOR ONLY 2 GAMES
 
 
