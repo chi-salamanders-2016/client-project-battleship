c5a535c46f61be0a38e2b4f2b56205d4d134f439
Matthew Behrens <mattbehrens96@gmail.com>
2016-09-16 13:41:58 -0500

    leaderboard finished

--- shattlebip/app/models/game.rb
+++ shattlebip/app/models/game.rb
@@ -7,9 +7,28 @@ class Game < ActiveRecord::Base
 
   def total_shots_for_game(user)
     boards = self.boards
-    other_board = boards.select {|board| board.user_id != user.id}
-    other_board = other_board[0]
+    other_board = boards.select {|board| board.user_id != user.id}[0]
     return other_board.shots.count
   end	
+
+  def total_hits(user)
+  	counter = 0
+  	boards = self.boards
+    other_board = boards.select {|board| board.user_id != user.id}[0]
+    other_board.shots.each do |shot|
+    	other_board.boats.each do |boat|
+    		if boat.cells.include?(shot.target_index)
+    			counter += 1
+    		end
+    	end
+    end
+    return counter 
+  end
+
+  def winner(user)
+  	if total_hits(user) >= 17
+  		return true
+  	end
+  end
 
 end
--- shattlebip/db/seeds.rb
+++ shattlebip/db/seeds.rb
@@ -1,14 +1,30 @@
-User.create(username: "User1", email: "User1@gmail.com", password: "password1234")
-User.create(username: "User2", email: "User2@gmail.com", password: "password1234")
-User.create(username: "User3", email: "User3@gmail.com", password: "password1234")
-User.create(username: "User4", email: "User4@gmail.com", password: "password1234")
+User.create(username: "Matt Baker", email: "User1@gmail.com", password: "password1234")
+User.create(username: "Duke", email: "User2@gmail.com", password: "password1234")
+User.create(username: "Derick", email: "User3@gmail.com", password: "password1234")
+User.create(username: "Paul", email: "User4@gmail.com", password: "password1234")
 User.create(username: "User5", email: "User5@gmail.com", password: "password1234")
-User.create(username: "User5", email: "User6@gmail.com", password: "password1234")
+User.create(username: "User6", email: "User6@gmail.com", password: "password1234")
 
 Game.create(name: "Test Game")
+Game.create(name: "Other Game")
+Game.create(name: "DBC Game")
+Game.create(name: "MY Game")
+Game.create(name: "Game")
+Game.create(name: "Other Game")
+
 
 Board.create(user_id: 1, game_id: 1)
 Board.create(user_id: 2, game_id: 1)
+Board.create(user_id: 3, game_id: 2)
+Board.create(user_id: 4, game_id: 2)
+Board.create(user_id: 1, game_id: 3)
+Board.create(user_id: 2, game_id: 3)
+Board.create(user_id: 4, game_id: 4)
+Board.create(user_id: 5, game_id: 4)
+Board.create(user_id: 6, game_id: 5)
+Board.create(user_id: 1, game_id: 5)
+Board.create(user_id: 2, game_id: 6)
+Board.create(user_id: 1, game_id: 6)
 
 Boat.create(name: "Carrier", length: 5, bow_index: 64, stern_index: 69, board_id: 1)
 Boat.create(name: "Battleship", length: 4, bow_index: 54, stern_index: 58, board_id: 1)
@@ -22,8 +38,8 @@ Boat.create(name: "Cruiser", length: 3, bow_index: 34, stern_index: 37, board_id
 Boat.create(name: "Submarine", length: 3, bow_index: 24, stern_index: 27, board_id: 2)
 Boat.create(name: "Destroyer", length: 2, bow_index: 14, stern_index: 16, board_id: 2)
 
-100.times do
-	Shot.create(board_id: rand(1..2), target_index:rand(1..99))
+1000.times do
+	Shot.create(board_id: rand(1..12), target_index:rand(1..99))
 end
 
 
