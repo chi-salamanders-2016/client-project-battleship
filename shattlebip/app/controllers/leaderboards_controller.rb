33feffbec738140f85a82ab8bac60a200e723937
Matthew Behrens <mattbehrens96@gmail.com>
2016-09-16 14:10:05 -0500

    fix a few bugs

--- shattlebip/app/controllers/leaderboards_controller.rb
+++ shattlebip/app/controllers/leaderboards_controller.rb
@@ -2,7 +2,7 @@ class LeaderboardsController < ApplicationController
 	def index
 		@most_user_wins = User.all.sort {|a,b| a.games_won <=> b.games_won}.reverse
 		@most_user_shots = User.all.sort {|a,b| a.total_shots_fired <=> b.total_shots_fired}.reverse
-		@most_user_win_ratio = User.all.sort {|a,b| a.total_shots_fired <=> b.total_shots_fired}.reverse
+		@most_user_win_ratio = User.all.sort {|a,b| a.win_loss_ratio <=> b.win_loss_ratio}.reverse
 		@most_user_accuracy = User.all.sort {|a,b| a.calculate_accuracy <=> b.calculate_accuracy}.reverse
 	end
 end
