e67fce366bb364b1df8ed4d6f8fc551b1bb3b734
Laurie Skelly <laurieskelly@users.noreply.github.com>
2016-09-16 01:07:51 -0500

    place your boats!

--- shattlebip/app/controllers/boards_controller.rb
+++ shattlebip/app/controllers/boards_controller.rb
@@ -2,10 +2,8 @@ class BoardsController < ApplicationController
 
   def edit
     @board = Board.find(params[:id])
-
     @boats = @board.get_or_init_fleet
-
   end
 
-
+  
 end
--- shattlebip/app/helpers/boards_helper.rb
+++ shattlebip/app/helpers/boards_helper.rb
@@ -1,2 +1,13 @@
 module BoardsHelper
+
+  def cell_class(row, column)
+    class_string = "cell "
+    index = row*10 + column
+    if @board.all_cells.keys.include? index
+      class_string << "assigned #{@board.all_cells[index]}"
+    else
+      class_string << "eligible bow"
+    end
+    class_string
+  end
 end
