78d474361db03c8116d4954d1193a655276f0a02
Matthew Behrens <mattbehrens96@gmail.com>
2016-09-16 14:51:00 -0500

    fix register page and email validation

--- shattlebip/app/controllers/users_controller.rb
+++ shattlebip/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to @user, notice: "user was saved"
 		else
 			render :new
